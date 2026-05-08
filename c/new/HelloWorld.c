701a8ff06282ae08c838009793f0e9e9924301b2
ashfaqkhan111 <ahfaqkhan97@gmail.com>
2026-05-08 10:39:48 +0700

    new update

--- c/new/HelloWorld.c
+++ c/new/HelloWorld.c
@@ -5,6 +5,10 @@ int main (){
     printf ("hi\nkhan\n     ashfaq\n");
 
     int a = 10;
-    printf ("%d",a);
+    float b=2.2;
+    char c = 'k';
+    printf ("%d\n",a);
+    printf ("%f\n",b);
+    printf ("%c\n",c);
     return 0;
 }
