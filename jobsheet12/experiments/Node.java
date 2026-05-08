3e185e8ae2f6d643dba71320f167e29ff3a1a112
ashfaqkhan111 <ahfaqkhan97@gmail.com>
2026-05-08 18:45:15 +0700

    jobsheet 12

---
+++ jobsheet12/experiments/Node.java
@@ -0,0 +1,15 @@
+package jobsheet12.experiments;
+
+public class Node {
+    Student data;
+    Node next;
+
+Node(){}
+
+Node (Student data, Node next){
+    this.data = data;
+    this.next = next;
+
+}
+    
+}
